cd216c7154a917792b3c4115b0a16d59bacb74bd
Kyle Ellrott <kellrott@gmail.com>
2015-12-23 00:14:09 -0800

    Changing how directory_hash_id deals with UUIDs

--- galaxy/util/directory_hash.py
+++ galaxy/util/directory_hash.py
@@ -9,15 +9,21 @@ def directory_hash_id( id ):
     ['090']
     >>> directory_hash_id("777777777")
     ['000', '777', '777']
+    >>> directory_hash_id("135ee48a-4f51-470c-ae2f-ce8bd78799e6")
+    ['13']
     """
     s = str( id )
     l = len( s )
     # Shortcut -- ids 0-999 go under ../000/
     if l < 4:
         return [ "000" ]
-    # Pad with zeros until a multiple of three
-    padded = ( ( 3 - len( s ) % 3 ) * "0" ) + s
-    # Drop the last three digits -- 1000 files per directory
-    padded = padded[:-3]
-    # Break into chunks of three
-    return [ padded[ i * 3 : (i + 1 ) * 3 ] for i in range( len( padded ) // 3 ) ]
+    if l < 10:
+        # Pad with zeros until a multiple of three
+        padded = ( ( 3 - len( s ) % 3 ) * "0" ) + s
+        # Drop the last three digits -- 1000 files per directory
+        padded = padded[:-3]
+        # Break into chunks of three
+        return [ padded[ i * 3 : (i + 1 ) * 3 ] for i in range( len( padded ) // 3 ) ]
+    else:
+        #assume it is a UUID
+        return [ s[0:2] ]
